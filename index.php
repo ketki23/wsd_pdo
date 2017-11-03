a2415ba22c9ae7d9b6e44752d1864d7b7d51566f
Ketki R Andhale <ketki.andhale@gmail.com>
2017-11-02 20:24:56 -0400

    connection

--- index.php
+++ index.php
@@ -7,10 +7,17 @@ try {
     $conn = new PDO($servername, $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
+    echo "Connected successfully<br><br><br>"; 
     }
 catch(PDOException $e)
     {
     echo "Connection failed: " . $e->getMessage();
     }
-?>
+
+  $sqlresult =$conn->query("select * from accounts");
+  
+  foreach ($sqlresult as $row)
+  {
+  	echo "<tr><td>{$row['id']}</td><td>{$row['email']}</td><td>{$row['fname']}</td><td>{$row['lname']}</td><td>{$row['phone']}</td><td>{$row['birthday']}</td><td>{$row['gender']}</td><td>{$row['password']}</td></tr>";
+  }
+  ?>
