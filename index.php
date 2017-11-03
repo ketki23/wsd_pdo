f59508b7c3bc14a4ab21e86f16ce22b9046b1665
Ketki R Andhale <ketki.andhale@gmail.com>
2017-11-02 20:30:18 -0400

    connection

--- index.php
+++ index.php
@@ -14,8 +14,11 @@ catch(PDOException $e)
     echo "Connection failed: " . $e->getMessage();
     }
 
-  $sqlresult =$conn->query("select * from accounts");
-  
+  $sqlresult =$conn->query("select * from accounts where id<6");
+
+  echo "<table border='1'>";
+   echo "<tr><td>id</td><td>email</td><td>fname</td><td>lname</td><td>phone</td><td>birthday</td><td>gender</td><td>password</td></tr>";
+
   foreach ($sqlresult as $row)
   {
   	echo "<tr><td>{$row['id']}</td><td>{$row['email']}</td><td>{$row['fname']}</td><td>{$row['lname']}</td><td>{$row['phone']}</td><td>{$row['birthday']}</td><td>{$row['gender']}</td><td>{$row['password']}</td></tr>";
